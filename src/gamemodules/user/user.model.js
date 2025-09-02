5c84fa71455454c125e9453f486175716e45ba22
CHAMRTHI VENKATESH <venkateshchamarthi412@gmail.com>
2025-09-02 10:51:00 +0530

    user login + signup + reset password done

--- src/gamemodules/user/user.model.js
+++ src/gamemodules/user/user.model.js
@@ -1,30 +1,63 @@
-const mongoose = require("mongoose");
+// models/User.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, "Name is required"],
+      required: [true, 'Name is required'],
     },
     email: {
       type: String,
-      required: [true, "Email is required"],
+      required: [true, 'Email is required'],
       unique: true,
       lowercase: true,
     },
-    role: {
+    password: {
       type: String,
-      enum: ["Coach", "Fan", "Parent"],
-      default: "Fan",
+      required: [true, 'Password is required'],
+      minlength: 6,
     },
+    globalRole: {
+      type: String,
+      enum: ['Fan', 'Parent'],
+      default: 'Fan',
+    },
+    // Contextual Roles: linked to teams/games
+    teams: [
+      {
+        teamId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'Team',
+        },
+        role: {
+          type: String,
+          enum: ['Coach', 'Player'],
+        },
+      },
+    ],
     subscribedTeams: [
       {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Team",
+        ref: 'Team',
       },
     ],
   },
   { timestamps: true }
 );
 
-module.exports = mongoose.model("User", userSchema);
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Method to compare password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
+
+module.exports = mongoose.model('User', userSchema);
